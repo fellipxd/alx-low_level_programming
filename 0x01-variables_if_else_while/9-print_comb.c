baba4d4740c548f20946e9f4ebcb6dd2ba48d787
fellipxd <philipokedi@gmail.com>
2023-06-16 13:44:35 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,28 +4,33 @@
  * main - Entry point of the program
  *
  * Description:
- * Prints all possible combinations of single-digit numbers separated by ", "
- * in ascending order using the putchar function.
+ *   Prints all possible different combinations of two digits separated by ", "
+ *   in ascending order using the putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int digit; /* Variable to store the ASCII value of the digits */
+	int tens_digit; /* Variable for tens digit */
+	int ones_digit; /* Variable for ones digit */
 
-    /* Print the combinations of single-digit numbers */
-    for (digit = 48; digit <= 57; digit++)
-    {
-        putchar(digit);
-        if (digit != 57)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+	/* Print the combinations of two digits */
+	for (tens_digit = 0; tens_digit <= 8; tens_digit++)
+	{
+		for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+		{
+			putchar(tens_digit + '0');
+			putchar(ones_digit + '0');
+			if (tens_digit != 8 || ones_digit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n'); /* Print new line character */
+	putchar('\n'); /* Print new line character */
 
-    return (0);
+	return (0);
 }
 
