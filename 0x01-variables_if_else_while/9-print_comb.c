c93d23ac5bdba2d7850a1e3f81c5c960a309e4db
fellipxd <philipokedi@gmail.com>
2023-06-16 13:41:36 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,28 +3,29 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible combinations of single-digit numbers
- *              separated by ", " in ascending order, using the putchar function.
+ * Description:
+ * Prints all possible combinations of single-digit numbers separated by ", "
+ * in ascending order using the putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit; /* Variable to store the ASCII value of the digits */
+    int digit; /* Variable to store the ASCII value of the digits */
 
-	/* Print the combinations of single-digit numbers */
-	for (digit = 48; digit <= 57; digit++)
-	{
-		putchar(digit);
-		if (digit != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+    /* Print the combinations of single-digit numbers */
+    for (digit = 48; digit <= 57; digit++)
+    {
+        putchar(digit);
+        if (digit != 57)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	putchar('\n'); /* Print new line character */
+    putchar('\n'); /* Print new line character */
 
-	return (0);
+    return (0);
 }
 
