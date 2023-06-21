82e914d3b215c6342c4e6b0735d99bb68a8aed2e
fellipxd <philipokedi@gmail.com>
2023-06-21 09:13:43 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * main-entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n>0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	
+	if(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1)
+	}
+}
+		
