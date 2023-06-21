017aa86ce9235ac46de9dae9b233019dbdeb5b2e
fellipxd <philipokedi@gmail.com>
2023-06-21 09:26:17 -0700

    6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,13 +8,13 @@
 
 int print_sign(int n)
 {
-	if (n>0)
+	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
-	
-	if(n == 0)
+
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -26,4 +26,4 @@ int print_sign(int n)
 		return (-1);
 	}
 }
-		
+
